477fc5d8a9a05e2fe3be96de491296b96d195c1c
SSLiza <shajedasultanaliza2002@gmail.com>
2026-09-17 23:15:20 +0600

    data type learned

---
+++ dist/primitive.js
@@ -0,0 +1,8 @@
+"use strict";
+//JS-TS
+Object.defineProperty(exports, "__esModule", { value: true });
+// string number boolean undefined null
+let username = "Liza";
+username = "John Doe";
+console.log(username);
+//# sourceMappingURL=primitive.js.map
---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+//JS-TS
+
+// string number boolean undefined null
+
+let username: string = "Liza";
+username = "John Doe";
+
+let x; //any
+let y: any; //any
+ x=4;
+ x= "y"
+
+ let z:undefined;
+
+console.log(username);
