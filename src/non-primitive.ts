de34ed5a811773607419495903089dc65b2e2e25
SSLiza <shajedasultanaliza2002@gmail.com>
2026-09-18 23:55:06 +0600

    Add TypeScript object examples

--- src/non-primitive.ts
+++ src/non-primitive.ts
@@ -6,6 +6,46 @@ console.log(obj.name); // "Alice"
 
 let bazarList: string[] = ["eggs", "milk", "sugar"];
 
-// bazarList.push(true) error
+bazarList.push("bread"); // valid
+// bazarList.push(true) // error
 
-let mixedArr = ['eggs', 12, 'milk', 1, 'sugar']
+let mixedArr: (string | number)[] = ['eggs', 12, 'milk', 1, 'sugar']
+
+mixedArr.push(1);
+mixedArr.push('sugar');
+// mixedArr.push(false);  error
+
+let coordinates: [number, number] = [10, 20]; // tuple
+
+let couple: [string, string] = ["John", "Jane"]; // destructuring
+
+let [man, woman] = couple; // destructuring assignment
+
+console.log(man);  // "John"
+console.log(woman);  // "Jane"
+
+let nameAndRoll: [string, number] = ["Alice", 101]; // tuple with string and number
+console.log(nameAndRoll[0]); // "Alice"
+console.log(nameAndRoll[1]); // 101
+
+let destination: [string, string, number] = ["Paris", "London", 1000]; // tuple with three elements
+console.log(destination[0]); // "Paris"
+console.log(destination[1]); // "London"
+console.log(destination[2]); // 1000
+
+
+//reference type
+
+let user: {
+    firstName: string;
+    midname: string;
+    lastName: string;
+    age: number;
+    isStudent: boolean
+} = {
+    firstName: "Alice",
+    // midname: "B.",
+    lastName: "Smith",
+    age: 30,
+    isStudent: true
+} 
