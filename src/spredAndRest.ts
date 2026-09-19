0e9f355af0b191ee2dd3a9f1538cd97199e368a2
SSLiza <shajedasultanaliza2002@gmail.com>
2026-09-20 00:00:06 +0600

    spread and rest

---
+++ src/spredAndRest.ts
@@ -0,0 +1,8 @@
+const friends = ['Alice', 'Bob', 'Charlie'];
+
+const [firstFriend, secondFriend, thirdFriend] = friends;
+
+console.log(firstFriend); // Output: Alice
+console.log(secondFriend);
+ 
+
